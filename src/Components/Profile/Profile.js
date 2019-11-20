03fabab31e46d405a0a77cd4034ea660e2a2dbcc
Lorena <lorena.tacal@gmail.com>
2019-11-20 15:59:05 +0000

    Display profile once the user has completed the questionnaire

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,10 +1,63 @@
-import React from 'react'
-import Questionnaire from '../Questionnaire'
+import React from 'react'
+import Questionnaire from '../Questionnaire'
+import Styles from './profile.module.css'
+import { useFirebaseDatabaseWriters, useFirebaseCurrentUser, useFirebaseDatabaseValue } from 'fireact'
+import ReactCardFlip from 'react-card-flip'
+import Nav from '../Nav/Nav'
 
-function Profile(params) {
-    return (
-        <Questionnaire/>
-    )
+
+function Profile(params) {
+    const user = useFirebaseCurrentUser()
+    const uid = user ? user.uid : null
+    const currentUser = useFirebaseDatabaseValue(`users/${uid}/Profile`)
+    const [profileComplete, setProfileComplete] = React.useState(false)
+
+    React.useEffect(() => {
+       console.log(currentUser, 'currentUser') 
+       if (currentUser) {
+           //check if string properties are not equal to string
+           if (
+                currentUser.firstname !== '' &&
+                currentUser.surname !== '' &&
+                currentUser.dateOfBirth !== '' &&
+                currentUser.address !== '' &&
+                currentUser.postcode !== '' &&
+                currentUser.language !== '' &&
+                currentUser.yearsOfExperience !== '' 
+           ) {
+               setProfileComplete(true)
+           } 
+       }
+    }, [currentUser])
+
+    
+
+    if (profileComplete) {
+        return (
+            <div className={Styles.mainContainer}>
+                <Nav className={Styles.navContainer} currentPage='Profile'/>
+                <div className={Styles.cardContainer}>
+                    <h3>Your Profile:</h3>
+                    <p>firstname: {currentUser.firstname}</p>
+                    <p>surname: {currentUser.surname}</p>
+                    <p>date of Birth: {currentUser.dateOfBirth}</p>
+                    <p>Address: {currentUser.address}</p>
+                    <p>postcode: {currentUser.postcode}</p>
+                    <p>language: {currentUser.language}</p>
+                    <p>years of experience: {currentUser.yearsOfExperience}</p>
+                    <p>fun fact: {currentUser.funFact}</p>
+                    {/* <p>firstname: {currentUser.firstname}</p>
+                    <p>firstname: {currentUser.firstname}</p> */}
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <Questionnaire/>
+        )
+    }
+    
 }
 
-export default Profile
+export default Profile
+
