d7aae6a6468cd5243d942e636df1a2af70da1143
Lorena <lorena.tacal@gmail.com>
2019-11-20 13:48:47 +0000

    Add functionality for the card to be flipped

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,15 +2,30 @@ import React from 'react'
 import Styles from './card.module.css'
 import { Grid } from '@material-ui/core'
 import avatar from '../../Assets/7_avatar-512.png'
+import ReactCardFlip from 'react-card-flip';
 
 
 function Card() {
+    const [flipped, setFlipped] = React.useState(false)
     return (
-        <div className={Styles.cardContainer}>
-            <div className={Styles.avatarContainer}>
-                <img className={Styles.avatar} src={avatar}></img>
-            </div>
-            </div>
+        <div>
+            <ReactCardFlip isFlipped={flipped} flipDirection="vertical">
+                <button className={Styles.cardContainer} onClick={() => {
+                    setFlipped(true)
+                }}>
+                    <div className={Styles.avatarContainer}>
+                        <img className={Styles.avatar} src={avatar}></img>
+                    </div>
+                </button>
+                <button className={Styles.cardContainer}  onClick={() => {
+                    setFlipped(false)
+                }}>
+                    <div className={Styles.avatarContainer}>
+                        <p>Back Card</p>
+                    </div>
+                </button>
+            </ReactCardFlip>
+        </div>
     )
 }
 
