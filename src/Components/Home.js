cb771a75a460deab73e5ba905d96b96c71a98500
CallumIllsley <callumillsley1324@gmail.com>
2019-11-20 10:05:23 +0000

    working links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,31 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import Styles from './home.module.css'
-import { logoutUser } from '../Redux/Actions/auth'
-import { useDispatch } from 'react-redux'
-import Nav from './Nav/Nav'
-import Card from './Card/Card'
-import Profile from './Profile/Profile'
+import Dash from './Dash'
+import Profile from '../Components/Profile/Profile'
+
 
 function Home() { 
-
-    const dispatch = useDispatch()
-    
-
     return (
-        <>
-            <Switch>
-                <Route path='/profile' component={Profile}/>
-            </Switch>
-            <div className={Styles.mainContainer}>
-                <div className={Styles.navContainer}>
-                    <Nav currentPage='Home'/>
-                </div>
-                <div className={Styles.cardContainer}>
-                    <Card></Card>
-                </div>
-            </div>
-        </>
+        <Switch>
+            <Route path='/profile' component={Profile}/>
+            <Route path='/' component={Dash}/>
+        </Switch> 
     )
 }
 
